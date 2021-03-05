0424cb1a06703c5f89fcb6fec7e4a51eecb93968
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2021-03-04 22:18:03 -0800

    Se hicieron modificaciones al diseño del navbar. Se creo una base de datos de prueba. Se comenzo con la creacion de los php para el login.

---
+++ codes/php/dataBaseLogin.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$user = "omdprofx_guerra-c";
+$password="Guel1870";
+$bd="omdprofx_guerra-c-ordinario";
+ ?>
---
+++ codes/sql/Sistema-EmisionesV1.sql
@@ -0,0 +1,45 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- -----------------------------------------------------
+-- Empresa
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS Empresa;
+CREATE TABLE Empresa (
+  idEmpresa INT NOT NULL,
+  Nombre VARCHAR(45) NULL,
+  Telefono VARCHAR(45) NULL,
+  Correo VARCHAR(45) NULL,
+  PRIMARY KEY (idEmpresa)
+  ) ;
+
+-- -----------------------------------------------------
+-- Usuario
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS Usuario;
+CREATE TABLE Usuario (
+  idUsuario INT NOT NULL,
+  Username VARCHAR(10) NOT NULL,
+  Password VARCHAR(45) NOT NULL,
+  Nombre VARCHAR(45) NOT NULL,
+  Ciudad VARCHAR(20) NOT NULL,
+  Correo VARCHAR(45) NOT NULL,
+  Telefono VARCHAR(45) NOT NULL,
+  Empresa_idEmpresa INT NOT NULL,
+  FOREIGN KEY (Empresa_idEmpresa)
+  REFERENCES Empresa(idEmpresa),
+  PRIMARY KEY (idUsuario)
+  ) ;
+
+
+-- -----------------------------------------------------
+-- Administrador
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS Administrador;
+CREATE TABLE Administrador (
+  idAdministrador INT NOT NULL,
+  Username VARCHAR(45) NOT NULL,
+  Password VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idAdministrador)
+  ) ;
+
+SET FOREIGN_KEY_CHECKS = 1;
