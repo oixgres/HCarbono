e3ff6fac2eed787571ef33e24e04efc20bc761ed
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2021-05-14 09:17:14 -0700

    Modificaciones para la pagina de registro de usuarios (admin)

--- SistemaEmisiones/codes/js/checkInput.js
+++ SistemaEmisiones/codes/js/checkInput.js
@@ -5,17 +5,29 @@ const errorMessage = document.getElementsByClassName('empty-input-message');
 /* Inputs del usuario */
 const input = document.getElementsByClassName('no-empty-input');
 
+/* Inputs necesarias para enviar correo */
 const sendMail = document.getElementById('sendMailSection');
 const mailRequirements = document.getElementsByClassName('required-for-mail');
-const errorMailMessage = document.getElementsByClassName('en-espera')
+const errorMailMessage = document.getElementsByClassName('required-for-mail-message');
 
-/* Agregamos eventListener a todos los input para que cuando un error sea modificado desaparezcan las señales */
+/* Agregamos eventListener a todos los input para que cuando un error sea modificado desaparezcan las señales de error */
 for(let i = 0; i < input.length; i++)
 {
   input[i].addEventListener('input', ()=>{
     if (input[i].value != '' && input[i].value != null){
       input[i].classList.remove("error");
       errorMessage[i].classList.remove("error");
+    }
+  })
+}
+
+/* Al ingresar datos se eliminan los mensajes de error */
+for(let i = 0; i < mailRequirements.length; i++)
+{
+  mailRequirements[i].addEventListener('input', ()=>{
+    if(mailRequirements[i].value != '' && mailRequirements[i].value != null){
+      mailRequirements[i].classList.remove('error');
+      errorMailMessage[i].classList.remove('error');
     }
   })
 }
@@ -35,12 +47,13 @@ form.addEventListener('submit', (e)=>{
     }
   }
 
-  /**/
+  /* Si se selecciono enviar correo verificamos que los campos username y password esten completos */
   if(sendMail.checked){
     for(var i = 0; i < mailRequirements.length; i++)
     {
-      if(mailRequirements[i].value  === '' || mailRequirements[i].value == null){
+      if(mailRequirements[i].value  === '' || mailRequirements[i].value == null || mailRequirements[i].checked==false){
         mailRequirements[i].classList.add("error");
+        errorMailMessage[i].classList.add("error");
   
         sendMail.checked = false;
         e.preventDefault();
@@ -59,8 +72,9 @@ sendMail.addEventListener('click', (e)=>{
 
   for(var i = 0; i < mailRequirements.length; i++)
   {
-    if(mailRequirements[i].value  === '' || mailRequirements[i].value == null){
+    if(mailRequirements[i].value  === '' || mailRequirements[i].value == null || (mailRequirements[i].type == "checkbox" && mailRequirements[i].checked == false)){
       mailRequirements[i].classList.add("error");
+      errorMailMessage[i].classList.add("error");
 
       errorCount++;
     }
