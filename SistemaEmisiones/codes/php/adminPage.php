6cb44919de586fbc104b79e7222713cfa603cc02
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2021-07-15 23:44:36 -0700

    Error de admin al crear usuarios corregido, hacer mas pruebas a futuro

--- SistemaEmisiones/codes/php/adminPage.php
+++ SistemaEmisiones/codes/php/adminPage.php
@@ -3,6 +3,7 @@ require_once "phpFunctions.php";
 
 checkSession('admin', "../../index.html");
 ?>
+
 <!DOCTYPE html>
 
 <html lang="en" dir="ltr">
@@ -10,14 +11,27 @@ checkSession('admin', "../../index.html");
     <meta charset="utf-8">
     <title>H. Carbono | Administrador</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+    <link 
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+      crossorigin="anonymous"
+    >
+    <script 
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+      crossorigin="anonymous"
+    ></script>
 
+    <!-- Originales -->
     <link rel="stylesheet" href="../css/pageStyle.css">
     <link rel="stylesheet" href="../css/popupStyle.css">
   </head>
 
   <body class="background-color">
+    
+    <!-- Navegador de la pagina -->
     <nav class="navbar navbar-dark config-color">
       <div class="container-fluid">
         <a class="navbar-brand" href="../../index.html">H.CARBONO</a>
@@ -27,76 +41,91 @@ checkSession('admin', "../../index.html");
       </div>
     </nav>
 
-    <section>
-      <?php
-        require_once "dataBaseLogin.php";
-        require_once "processCRUD.php";
-        require_once "phpFunctions.php";
+    <?php
+      require_once "dataBaseLogin.php";
 
-        $result = mysqli_query($connection, "SELECT * FROM Usuario");
-      ?>
+      /* Funciones CRUD */
+      require_once "processCRUD.php";
 
-      <!-- Tabla para mostrar los usuarios registrados -->
-      <div class="mx-5 mt-5 table-responsive">
-        <table class="table">
-          <!--Cabeza de la tabla-->
-          <thead>
-            <tr>
-              <th>Usuario</th>
-              <th>Contraseña</th>
-              <th>Nombre</th>
-              <th>Ciudad</th>
-              <th>Correo</th>
-              <th>Telefono</th>
-              <th>Estado</th>
-              <th>Empresa</th>
-              <th>Dispositivo</th>
-              <th>Accion</th>
-            </tr>
-          </thead>
+      /* Obtenemos los usuarios */
+      $result = mysqli_query($connection, "SELECT * FROM Usuario");
+    ?>
 
-          <?php while($row = mysqli_fetch_assoc($result)): ?>
-            <tr>
-              <td><?php echo $row['Username']; ?></td>
-              <td><?php echo $row['Password']; ?></td>
-              <td><?php echo $row['Nombre']; ?></td>
-              <td><?php echo $row['Ciudad']; ?></td>
-              <td><?php echo $row['Correo']; ?></td>
-              <td><?php echo $row['Telefono']; ?></td>
-              <td><?php echo $row['Aprobado']; ?></td>
+    <!-- Tabla para mostrar los usuarios registrados -->
+    <div class="mx-5 mt-5 table-responsive">
+      <table class="table">
+        
+        <!--Cabeza de la tabla-->
+        <thead>
+          <tr>
+            <th>Usuario</th>
+            <th>Contraseña</th>
+            <th>Nombre</th>
+            <th>Ciudad</th>
+            <th>Correo</th>
+            <th>Telefono</th>
+            <th>Estado</th>
+            <th>Empresa</th>
+            <th>Dispositivo</th>
+            <th>Accion</th>
+          </tr>
+        </thead>
 
-              <!-- Obtenemos el nombre de la empresa -->
-              <?php
-                $company = getFirstQueryElement($connection, "Empresa", "Nombre", "idEmpresa", $row['Empresa_idEmpresa']);
-              ?>
-              <td><?php echo  $company?></td>
+        <!-- Renglones de la tabla -->
+        <?php while($row = mysqli_fetch_assoc($result)): ?>
+          <tr>
+            <td><?php echo $row['Username']; ?></td>
+            <td><?php echo $row['Password']; ?></td>
+            <td><?php echo $row['Nombre']; ?></td>
+            <td><?php echo $row['Ciudad']; ?></td>
+            <td><?php echo $row['Correo']; ?></td>
+            <td><?php echo $row['Telefono']; ?></td>
+            <td><?php echo $row['Aprobado']; ?></td>
 
-              <?php
-                $device = getFirstQueryElement($connection, "Dispositivo", "Codigo", "Usuario_idUsuario", $row['idUsuario'])
-              ?>
-              <td><?php echo $device; ?></td>
+            <!-- Obtenemos el nombre de la empresa -->
+            <?php
+              $company = getFirstQueryElement(
+                $connection,
+                "Empresa",
+                "Nombre",
+                "idEmpresa",
+                $row['Empresa_idEmpresa']
+              );
+            ?>
+            <td><?php echo  $company; ?></td>
 
-              <td>
-                <div class="d-flex justify-content-end">
-                  <!-- Redireccion a pagina para editar el usuaro -->
-                  <a
-                    href="adminPage.php?edit=<?php echo $row['idUsuario']; ?>"
-                    class="btn config-button">Editar
-                  </a>
+            <!-- Obtenemos el codigo del dispositivo -->
+            <?php
+              $device = getFirstQueryElement(
+                $connection,
+                "Dispositivo",
+                "Codigo",
+                "Usuario_idUsuario",
+                $row['idUsuario']
+              );
+            ?>
+            <td><?php echo $device; ?></td>
 
-                  <a
-                    id="adminPage.php?delete=<?php echo $row['idUsuario']; ?>"
-                    onclick="toggleDeletePopup(this)"
-                    class="btn config-button-danger mx-3"
-                  >Borrar
-                  </a>
-                </div>
-              </td>
-            </tr>
-          <?php endwhile; ?>
-        </table>
-      </div>
-    </section>
+            <td>
+              <div class="d-flex justify-content-end">
+                <!-- Editar usuaro -->
+                <a
+                  href="adminPage.php?edit=<?php echo $row['idUsuario']; ?>"
+                  class="btn config-button"
+                >Editar</a>
+
+                <!-- Eliminar Usuario -->
+                <a
+                  id="adminPage.php?delete=<?php echo $row['idUsuario']; ?>"
+                  onclick="toggleDeletePopup(this)"
+                  class="btn config-button-danger mx-3"
+                >Borrar</a>
+              </div>
+            </td>
+          </tr>
+        <?php endwhile; ?>
+      </table>
+    </div>
 
     <!-- Boton para crear usuarios -->
     <div class="d-flex justify-content-end">
